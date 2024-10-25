2f04d07e34b43f27b207803ebb4a1c947b30d554
Freeda Vales <freedavales28@gmail.com>
2024-10-25 23:35:08 +0530

    Connected to MongoDb

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 import express from 'express';
 import 'dotenv/config';
+import { connectDB } from './db/connect.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import { notFound } from './middleware/notFound.js';
+import productRoutes from './routes/products.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const start = () => {
-  //   try {
-  //     await connectDB(connectionString);
-  app.listen(PORT, () => {
-    console.log('Listening...');
-  });
-  //   } catch (e) {
-  //     console.log(e);
-  //   }
+const start = async (connectionString) => {
+  try {
+    await connectDB(connectionString);
+    app.listen(PORT, () => {
+      console.log('Listening...');
+    });
+  } catch (e) {
+    console.log(e);
+  }
 };
 
-start();
+start(process.env.MONGO_URI);
 app.use(express.json());
+
+app.use('/api/v1/products', productRoutes);
 app.use(notFound);
 app.use(errorHandler);
